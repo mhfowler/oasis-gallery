59c7f502b00e97f8db4af0d8f1f4c17eee79c858
Max Fowler <maxhfowler@gmail.com>
2016-11-02 01:44:38 -0400

    run go

--- go/go.py
+++ go/go.py
@@ -3,9 +3,11 @@
 # If user types "go", the Go will print "Good."
 # If user types any other instruction except "go", then Go will print "We have to go."
 # So user have to keep going like our lives.
-
 def go(word):
 	if word == "go":
 		return "Good."
 	else:
 		return "We have to go."
+
+while True:
+	print go(raw_input())
