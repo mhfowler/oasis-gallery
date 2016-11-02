ec18528ca5968aeb44aa6f85fcb1cb0c11fe7633
Max Fowler <maxhfowler@gmail.com>
2016-11-02 01:40:09 -0400

    run go

--- go/go.py
+++ go/go.py
@@ -1,3 +1,4 @@
+
 # I made this poem with Python. The title is "Go". Go is a very simple program with only instruction "go".
 # If user types "go", the Go will print "Good."
 # If user types any other instruction except "go", then Go will print "We have to go."
